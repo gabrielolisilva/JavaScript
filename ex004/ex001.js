de77ec0273ae56a56ffd66536bcede80e39c18d5
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-07-28 15:18:14 -0300

    Exercício Condições

---
+++ ex004/ex001.js
@@ -0,0 +1,12 @@
+var vel = 91
+console.log(`Seu carro está andando a ${vel}Km/h`)
+if(vel >= 90){
+    console.log('Você está acima da velocidade permitida, MULTADO!!!')
+}
+
+var país = "França"
+if(país == "Brasil"){
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é Estrangeiro')
+}

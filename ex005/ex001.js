cb5c0c12243d9ecdcc9863f0a23d270d36740d4e
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-07-29 15:21:44 -0300

    Exercícios Repetições

---
+++ ex005/ex001.js
@@ -0,0 +1,23 @@
+//while
+
+var c = 1
+
+while(c <= 10){
+    console.log(`Olá, Mundo ${c}`)
+    c++
+}
+
+//do while
+
+var b = 1
+
+do{
+    console.log('Olá, Mundo')
+    b++
+} while(b <= 10)
+
+//for
+
+for(var a = 1; a<=10;a++){
+    console.log('Olá, tudo bem?')
+}

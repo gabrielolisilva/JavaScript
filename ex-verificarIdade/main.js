4a8dd141392518780b358caaeed8e0a7ef9f5547
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-07-29 11:31:50 -0300

    Exercício Verificador de Idade (parte 1)

---
+++ ex-verificarIdade/main.js
@@ -0,0 +1,39 @@
+function verificador(){
+    var textoAlterado = document.querySelector('#fraseFinal')
+
+    var agora = new Date()
+    var anoAtual = agora.getFullYear()
+
+    var dado = document.querySelector('#data')
+    var valorDigitado = Number(dado.value)
+    var idade = anoAtual - valorDigitado
+
+    var fsex = document.getElementsByName('sexo')
+    var genero= ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if(valorDigitado < 1920 || valorDigitado > anoAtual){
+        window.alert('Insira uma data válida!')
+    } else{
+    if(fsex[0].checked){
+        genero = 'Homem'
+        if(idade >= 0 && idade < 10){
+            //criança
+            img.setAttribute('src', 'imagens/crianca-m.jpg')
+        } else if(idade >=10 && idade < 21){
+            //adolescente
+            img.setAttribute('src', 'imagens/adolescente-m.jpg')
+        } else if(idade < 50){
+            //adulto
+        } else{
+            //idoso
+        }
+    } else if(fsex[1].checked){
+        genero = 'Mulher'
+    }
+    textoAlterado.style.textAlign= 'center'
+    textoAlterado.innerHTML= `Detectamos 
+    um ${genero} de ${idade} Anos`
+    textoAlterado.appendChild(img)
+    }
+}

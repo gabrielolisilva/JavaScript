cf6bcf72608086b08c6b072d1fe811ab438d64c3
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-07-29 09:20:45 -0300

    Exercício Horário

--- ex-Horário/main.js
+++ ex-Horário/main.js
@@ -5,14 +5,15 @@ var frase = window.document.querySelector('#dataFrase')
 frase.innerHTML= `Agora são ${horaAtual} Horas`
 
 var corFundo = window.document.querySelector('#fundo')
-var imagemBola = window.document.querySelector('#Foto')
+var imagemBola = window.document.querySelector('#img')
 
 if(horaAtual < 12){
     corFundo.style.background = 'lightgray'
-    imagemBola.style.background.image = 'imagens/foto-manha.jpg'
+    imagemBola.src = 'imagens/foto-manha.jpg'
 } else if(horaAtual <= 18){
     corFundo.style.background = 'FFFFe0'
-    imagemBola.style.background.image = 'imagens/foto-tarde.jpg'
+    imagemBola.src = 'imagens/foto-tarde.jpg'
 } else{
     corFundo.style.background = '#964B00'
+    imagemBola.src = 'imagens/foto-noite.jpg'
 }

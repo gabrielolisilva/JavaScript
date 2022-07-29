786ea1eead9656d849e49f0f4887cd5513429701
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-07-29 09:25:43 -0300

    Alterações

--- ex-Horário/main.js
+++ ex-Horário/main.js
@@ -4,16 +4,15 @@ var horaAtual = agora.getHours()
 var frase = window.document.querySelector('#dataFrase')
 frase.innerHTML= `Agora são ${horaAtual} Horas`
 
-var corFundo = window.document.querySelector('#fundo')
 var imagemBola = window.document.querySelector('#img')
 
 if(horaAtual < 12){
-    corFundo.style.background = 'lightgray'
+    document.body.style.background = 'lightgray'
     imagemBola.src = 'imagens/foto-manha.jpg'
 } else if(horaAtual <= 18){
-    corFundo.style.background = 'FFFFe0'
+    document.body.style.background = 'FFFFe0'
     imagemBola.src = 'imagens/foto-tarde.jpg'
 } else{
-    corFundo.style.background = '#964B00'
+    document.body.style.background = '#964B00'
     imagemBola.src = 'imagens/foto-noite.jpg'
 }

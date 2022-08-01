e29af92dc735f9638cfb3e9f082964be4b87eb7d
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-08-01 09:29:33 -0300

    Exercício Contador

--- ex-Contar/main.js
+++ ex-Contar/main.js
@@ -9,7 +9,25 @@ function contando(){
     var textoMudou = document.querySelector('#textoAlterado')
 
 
-    for (valorInicio; valorInicio <= valorFim; valorInicio++){
-        textoMudou.innerHTML= `${valorInicio}`
+    if(dadoInicio.value.length == 0 || dadoFim.value.length == 0 || dadoPasso.value.length == 0){
+        window.alert('Dados Faltando, completo todos os campos!')
+    } else{
+        textoMudou.innerHTML= 'Contando... <br>'
+
+        if(valorPasso <= 0){
+            window.alert('Passo inválido, considerando passo 1')
+            valorPasso = 1
+        }
+        if (valorInicio < valorFim){
+            for(let c = valorInicio; c <= valorFim; c += valorPasso){
+                textoMudou.innerHTML += `${c} \u{1F449}` //esse código de emoji só funciona entre crases
+            }
+            textoMudou.innerHTML += `\u{1F3C1}`
+        } else {
+            for(let c = valorInicio; c >= valorFim; c -= valorPasso){
+                textoMudou.innerHTML += `${c} \u{1F449}`
+            }
+            textoMudou.innerHTML += `\u{1F3C1}`
+        }      
     }
 }
